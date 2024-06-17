bc797eb75edfae0ddff62c8929e342f701bc2074
Lee-Ann Seegets <69766648+lseegets@users.noreply.github.com>
2024-06-17 18:58:37 +0200

    add hide list name button and basic pdf func

--- src/AppComponent/App.js
+++ src/AppComponent/App.js
@@ -2,13 +2,33 @@ import './App.css';
 import AddPoseForm from '../AddPoseFormComponent/AddPoseForm';
 import PoseList from '../PoseListComponent/PoseList';
 import { generateListId } from '../utilities';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export default function App() {
 
   const [showPoseNames, setShowPoseNames] = useState(true);
   const [lists, setLists] = useState([[]]);
   const [activeListIndex, setActiveListIndex] = useState(0);
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 30;
+      pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+      pdf.save('lesson.pdf');
+    })
+  }
 
   const addPose = (pose) => {
     setLists((prevLists) => {
@@ -65,15 +85,16 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <h1>Search Asanas to add them to your lesson</h1>
+        <button className="download-btn" onClick={downloadPDF}>Download PDF</button>
       </header>
+
       <main>
         <div className="input">
-        <button onClick={addList}>Add New List</button>
+        <button onClick={addList}>Add List</button>
         <AddPoseForm
           addPose={addPose}
           addList={addList}
         />
-
         <button
           className={`toggle-btn ${showPoseNames ? 'toggled' : ''}`}
           onClick={togglePoseNameVisibility}
@@ -83,11 +104,14 @@ export default function App() {
 
         </div>
 
-        <div className="lists-container">
+        <div
+          className="lists-container"
+          ref={pdfRef}
+        >
           {lists && lists.map((list, index) => (
               <PoseList
                 key={`pose-list-${index}`}
-                id={generateListId()}
+                //id={generateListId()}
                 index={index}
                 list={list}
                 setActiveList={setActiveList}
--- src/PoseListComponent/PoseList.js
+++ src/PoseListComponent/PoseList.js
@@ -9,7 +9,7 @@ export default function PoseList({ index, list, setActiveList, removePose, updat
 
     const [poses, setPoses] = useState([]);
     const [listName, setListName] = useState('New List');
-    const [isHovering, setIsHovering] = useState(false);
+    const [hideName, setHideName] = useState(true);
 
 
     useEffect(() => {
@@ -29,29 +29,46 @@ export default function PoseList({ index, list, setActiveList, removePose, updat
       setListName(sanitizeHtml(e.currentTarget.innerHTML));
     }
 
-    const handleHover = () => {
-      setIsHovering((prev) => !prev);
-      const visibility = !isHovering ? 'visible' : 'hidden';
+    const handleHover = (e) => {
+      const visibility = e.type === 'mouseenter' ? 'visible' : 'hidden';
       document.getElementById(`remove-ul-button-${index}`).style.visibility = visibility;
+      document.getElementById(`hide-btn-${index}`).style.visibility = visibility;
     };
 
     const handleContainerClick = () => {
       setActiveList(index);
     }
 
+    const toggleHideName = () => {
+      const visibility = !hideName ? 'visible' : 'hidden';
+      let listName = document.getElementById(`list-name-${index}`);
+      listName.style.visibility = visibility;
+      setHideName((prev) => !prev);
+    }
+
     return (
-      <h3
+      <div
         onClick={handleContainerClick}
         className={`container${activeListIndex === index ? '-active' : ''}`}
         onMouseEnter={handleHover}
         onMouseLeave={handleHover}
       >
-        <ContentEditable
-          className="list-name"
-          html={listName}
-          onChange={onListNameChange}
-          spellCheck="false"
-        />
+        <header>
+          <div
+            id={`hide-btn-${index}`}
+            className="hide-btn"
+            onClick={toggleHideName}
+          >
+            {hideName ? "(o)" : "(-)"}
+          </div>
+          <ContentEditable
+            className="list-name"
+            id={`list-name-${index}`}
+            html={listName}
+            onChange={onListNameChange}
+            spellCheck="false"
+          />
+        </header>
         <Reorder.Group
           axis="x" values={poses}
           onReorder={handleReorder}
@@ -73,7 +90,7 @@ export default function PoseList({ index, list, setActiveList, removePose, updat
             &times;
           </div>
         </Reorder.Group>
-      </h3>
+      </div>
     );
 }
 
